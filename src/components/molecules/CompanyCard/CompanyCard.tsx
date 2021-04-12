519148d18e7aa682f10fe2e7f98660887c3571ac
Donquixote Sabo <mixboksu@gmail.com>
2021-04-12 07:30:58 +0200

    feat: :sparkles: Move images to public file and import them to card and header

--- src/components/molecules/CompanyCard/CompanyCard.tsx
+++ src/components/molecules/CompanyCard/CompanyCard.tsx
@@ -1,8 +1,23 @@
-const CompanyCard: React.FC<Company> = ({ company, role }) => {
+//I will extract it to new files later :D
+import styled from 'styled-components';
+
+const Card = styled.li`
+  width: 100%;
+  height: 105px;
+  background-color: red;
+`;
+
+const Img = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+`;
+
+const CompanyCard: React.FC<Company> = ({ company, role, logo }) => {
   return (
-    <li>
-      {company}, {role}{' '}
-    </li>
+    <Card>
+      <Img src={process.env.PUBLIC_URL + logo} />
+    </Card>
   );
 };
 
--- src/components/molecules/ImageWrapper/ImageWrapper.tsx
+++ src/components/molecules/ImageWrapper/ImageWrapper.tsx
@@ -1,14 +1,17 @@
 import { Wrapper } from './ImageWrapper.styles';
-//images
-import mobileBgImage from 'assets/images/bg-header-mobile.svg';
-import desktopBgImage from 'assets/images/bg-header-desktop.svg';
 
 const ImageWrapper = () => {
   return (
     <Wrapper>
       <picture>
-        <source media='(min-width: 1440px)' srcSet={desktopBgImage} />
-        <img src={mobileBgImage} alt='header' />
+        <source
+          media='(min-width: 1440px)'
+          srcSet={process.env.PUBLIC_URL + '/images/bg-header-desktop.svg'}
+        />
+        <img
+          src={process.env.PUBLIC_URL + '/images/bg-header-mobile.svg'}
+          alt='header'
+        />
       </picture>
     </Wrapper>
   );
