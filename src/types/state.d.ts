259fee9c1b72d83d95df1497cf93a18c1c296d2c
Donquixote Sabo <mixboksu@gmail.com>
2021-04-12 07:06:33 +0200

    perf: :recycle: Extract Company into new file

--- src/types/state.d.ts
+++ src/types/state.d.ts
@@ -1,19 +1,3 @@
-interface Company {
-  id: number;
-  company: string;
-  logo: string;
-  new: boolean;
-  featured: boolean;
-  position: string;
-  role: string;
-  level: string;
-  postedAt: string;
-  contract: string;
-  location: string;
-  languages: string[];
-  tools: string[];
-}
-
 type State = {
   companies: Company[];
 };
