c2cb291b45427f11f364e66d8f9614bd58d21cce
Donquixote Sabo <mixboksu@gmail.com>
2021-04-12 06:56:17 +0200

    feat: :sparkles: Connect Redux to CompaniesList

--- src/components/organism/CompaniesList/CompaniesList.tsx
+++ src/components/organism/CompaniesList/CompaniesList.tsx
@@ -1,14 +1,23 @@
+import { connect } from 'react-redux';
+//components
 import CompanyCard from 'components/molecules/CompanyCard/CompanyCard';
-
+//styles
 import { Wrapper } from './CompaniesList.styles';
 
-function List() {
+const List: React.FC<State> = ({ companies }) => {
+  console.log(companies);
   return (
     <Wrapper>
       <CompanyCard text='SIEMAA KOMPANIA' />
       <CompanyCard text='CO tam u was' />
     </Wrapper>
   );
-}
+};
 
-export default List;
+const mapStateToProps = (state: State) => {
+  return {
+    companies: state.companies,
+  };
+};
+
+export default connect(mapStateToProps)(List);
