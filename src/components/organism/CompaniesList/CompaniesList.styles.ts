576e3f91dc09607b82ae10df41f875cc8d1ffc20
Donquixote Sabo <mixboksu@gmail.com>
2021-04-12 07:09:23 +0200

    refactor: :zap: Add semantic tags to CompanyList

--- src/components/organism/CompaniesList/CompaniesList.styles.ts
+++ src/components/organism/CompaniesList/CompaniesList.styles.ts
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.ul`
+  display: block;
   width: 100%;
   max-width: 1000px;
   padding: 50px;
   margin: 0 auto;
+
+  li {
+    margin: 20px 0;
+  }
 `;
