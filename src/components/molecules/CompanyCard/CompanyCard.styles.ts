89285bfc28c8f62eec14beb9c48df2c2c636d32d
Donquixote Sabo <mixboksu@gmail.com>
2021-04-13 12:05:15 +0200

    feat: :lipstick: Add hover effect on card and filters

--- src/components/molecules/CompanyCard/CompanyCard.styles.ts
+++ src/components/molecules/CompanyCard/CompanyCard.styles.ts
@@ -6,10 +6,16 @@ export const Card = styled.li`
   height: 105px;
   display: flex;
   align-items: center;
-  border-left: 4px solid ${({ theme }) => theme.colors.primary};
   border-radius: 4px;
   background-color: #fff;
-  box-shadow: 5px 5px 5px #ddd;
+
+  border-left: 4px solid transparent;
+  transition: 0.5s;
+
+  &:hover {
+    border-left: 4px solid ${({ theme }) => theme.colors.primary};
+    box-shadow: 7px 7px 7px #ddd;
+  }
 `;
 
 export const Img = styled.img`
